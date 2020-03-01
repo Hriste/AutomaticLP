019b23bf92128a90baeb1a1b55a5552e2a3f3cb4
hriste <christinalotr@gmail.com>
2020-02-29 19:41:49 -0500

    organize

--- MatlabImplementation/BoundingBoxes2.m
+++ MatlabImplementation/BoundingBoxes2.m
@@ -6,10 +6,18 @@ clear all
 % TODO: make conversion a function call
 % TODO: possibly call python script from matlab file
 trainingData = load('formattedData.mat').dsnew;
+%trainingData = load('formattedData.mat').sc;
+%trainingData = load('formattedData.mat').tc;
+
+% temp = load('imageLabelerOut2.mat').gTruth;
+% [imds, blds] = objectDetectorTrainingData(temp);
+% trainingData = combine(imds, blds);
 
 % Temporary Fix
 % TODO: make conversion spit out a formattedData with both datastores in it
 testData = load('tempTestData.mat').dsnew;
+%testData = load('tempTestData.mat').sc;
+%testData = load('tempTestData.mat').tc;
 
 % create YOLO v2 object detection network
 % TODO: if network dosen't exist run a functionized NetworkMaker.m
@@ -17,8 +25,7 @@ lgraph = load('customNetwork.mat').lgraph;
 
 % TODO: if no saved training file, train else use saved training
 % or at least something more automatic
-doTraining = false; 
-
+doTraining = true
 if doTraining
         % Configure the training options. 
     %  * Lower the learning rate to 1e-3 to stabilize training. 
@@ -26,18 +33,19 @@ if doTraining
     %    location. If training is interrupted due to a system failure or
     %    power outage, you can resume training from the saved checkpoint.
     options = trainingOptions('sgdm', ...
-        'MiniBatchSize', 4, ....
+        'MiniBatchSize', 5, ....
         'InitialLearnRate',1e-3, ...
-        'MaxEpochs',10);    
+        'MaxEpochs',20);    
     
     % Train YOLO v2 detector.
     [detector,info] = trainYOLOv2ObjectDetector(trainingData,lgraph,options);
-    save('trainedDetector.mat', detector);
+    
+    save('trainedDetector.mat', 'detector','info');
 else
     pretrained = load('trainedDetector.mat');
     detector = pretrained.detector;
 end
-
+testData = trainingData; 
 
 % Create a table to hold the bounding boxes, scores, and labels output by
 % the detector. 
@@ -46,7 +54,7 @@ results = table('Size',[numImages 3],...
     'VariableTypes',{'cell','cell','cell'},...
     'VariableNames',{'Boxes','Scores','Labels'});
 
-% Run detector on each image in the test set and collect results.
+% Run detector on each image in therre test set and collect results.
 for i = 1:numImages
     
     % Read the image.
@@ -71,8 +79,8 @@ imshow(I);
 % TODO: Fix
 % Below didn't love being a datastore that's ok for now
 % Extract expected bounding box locations from test data.
-%expectedResults = testData(:, 2:end);
+expectedResults = testData.UnderlyingDatastores{1,2};
 
 % Evaluate the object detector using average precision metric.
-%[ap, recall, precision] = evaluateDetectionPrecision(results, expectedResults);
+[ap, recall, precision] = evaluateDetectionPrecision(results, expectedResults);
 
