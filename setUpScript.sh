e5399ea9ad7e13b19015614b639df6fa46624cf9
Hriste <christinalotr@gmail.com>
2020-04-25 21:23:04 -0400

    clean up

--- setUpScript.sh
+++ setUpScript.sh
@@ -1,41 +1,45 @@
 #!/usr/bin/bash
 
-
-echo "This setup script assummes you've cloned the AutomaticLP Repo and are executing from user@jhu.edu/code/AutomaticLP."
-echo "Affirm this is the case:"
-select yn in "Yes" "No"; do
-  case $yn in
-    Yes ) break;;
-    No ) return;;
-  esac
-done
-
 module restore
 module load python/3.7-anaconda-2019.03
 module load cuda/9.0
 
+# Get Tensorflow Object Detection API Code
 git clone https://github.com/tensorflow/models
 
+# Get Forked Confusion Matrix Project
 git clone https://github.com/Hriste/tf_object_detection_cm.git
 
+# Confusion Matrix Project requires a path for tensorflow
 echo "Overwrite current tf_example_parser with modifications."
 cp tf_object_detection_cm/tf_example_parser.py models/research/object_detection/metrics/tf_example_parser.py
 
-conda deactivate
+# Install protobuf
+cd models/research
+FILE=protobuf.zip
+if [ ! -f "$FILE" ]; then
+    wget -O protobuf.zip https://github.com/google/protobuf/releases/download/v3.0.0/protoc-3.0.0-linux-x86_64.zip
+    unzip protobuf.zip
+fi
+./bin/protoc object_detection/protos/*.proto --python_out=.
+pip install --user .
+cd ..
 
-cd ../..
 
-conda create -p ./.conda/envs/LPEnvironment
-conda init bash
 
-conda activate LPEnvironment
+#conda deactivate
 
-conda install tensorflow-gpu=1.*
-conda install ipykernel
-conda install pillow
-conda install matplotlib
-conda install -c menpo opencv
-conda install pandas
-conda install scikit-learn
+#conda create -p ./envs/LPEnvironment
+#conda init bash
 
-ipython kernel install --user --name=LPKernel
+#conda activate ./envs/LPEnvironment
+
+#conda install tensorflow-gpu=1.*
+#conda install ipykernel
+#conda install pillow
+#conda install matplotlib
+#conda install -c menpo opencv
+#conda install pandas
+#conda install scikit-learn
+
+#ipython kernel install --user --name=LPKernel
