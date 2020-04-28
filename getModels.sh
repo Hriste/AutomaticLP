9d56068a8f008e3d86632cf5b274ae7b6530ee34
Hriste <christinalotr@gmail.com>
2020-04-28 17:29:08 -0400

    model download

---
+++ getModels.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+curl -O http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+
+tar -xzf faster_rcnn_inception_v2_coco_2018_01_28.tar.gz
+
+curl -O http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet101_coco_2018_01_28.tar.gz
+
+tar -xzf faster_rcnn_resnet101_coco_2018_01_28.tar.gz
+
+curl -O http://download.tensorflow.org/models/object_detection/ssd_inception_v2_coco_2018_01_28.tar.gz
+
+tar -xzf ssd_inception_v2_coco_2018_01_28.tar.gz
+
+curl -O http://download.tensorflow.org/models/object_detection/faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz
+
+tar -xzf faster_rcnn_inception_resnet_v2_atrous_coco_2018_01_28.tar.gz
+
+curl -O http://download.tensorflow.org/models/object_detection/faster_rcnn_nas_coco_2018_01_28.tar.gz
+
+tar -xzf faster_rcnn_nas_coco_2018_01_28.tar.gz
+
+curl -O http://download.tensorflow.org/models/object_detection/faster_rcnn_resnet50_coco_2018_01_28.tar.gz
+
+tar -xzf faster_rcnn_resnet50_coco_2018_01_28.tar.gz
+
+curl -O http://download.tensorflow.org/models/object_detection/rfcn_resnet101_coco_2018_01_28.tar.gz
+
+tar -xzf rfcn_resnet101_coco_2018_01_28.tar.gz
+
