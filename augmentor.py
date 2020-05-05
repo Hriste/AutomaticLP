2aa661a53a8600d06ec1fba87068e1a48bbecd7a
Hriste <christinalotr@gmail.com>
2020-05-05 19:48:32 -0400

    updates for motion blur

--- augmentor.py
+++ augmentor.py
@@ -5,9 +5,34 @@ import csv
 import imageio
 
 WHITE_LIST_FORMAT = ('png', 'jpg', 'jpeg', 'bmp', 'ppm', 'JPG')
+VALID_AUGMENTERS = ["jpegCompression", "MotionBlur"]
+
+def select_augmentation():
+    print("Valid Augmentation Values are:")
+    for entry in VALID_AUGMENTERS:
+        print(entry)
+
+    choosen = input("Please select what augmenation you would like to perform:")
+
+    if "jpegcompression" in choosen.lower():
+        level = int(input("Please enter a severity level between 1-5:"))
+        augmentor = iaa.imgcorruptlike.JpegCompression(severity=level)
+    elif "motionblur" in choosen.lower():
+        kernel = int(input("Enter the motion blur kernel size in pixels (suggestion 15):"))
+        # TODO: can add angle(s) for motion blur
+        augmentor = iaa.MotionBlur(k=kernel)
+    else:
+        augmentor = None
+
+    return augmentor
 
 def augment_images(path):
     rowsToAdd = []
+
+    augmentor = select_augmentation()
+    if augmentor is None:
+        print("No Augmentation Type selected")
+        return
 
     with open(path + "/dataset.csv") as csvfile:
         csvreader = csv.reader(csvfile)
@@ -23,8 +48,7 @@ def augment_images(path):
             rowsToAdd.append(new_row)
 
             ia.seed(1)
-            compression = iaa.imgcorruptlike.JpegCompression(severity=2)
-            image_aug = compression(image = image)
+            image_aug = augmentor(image = image)
             im_name = path + "/" + new_name
             imageio.imwrite(im_name, image_aug[:, :, 0])
 
